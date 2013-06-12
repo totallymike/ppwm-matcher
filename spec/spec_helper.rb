d7485299535c3c0f5b23e40210376a4a7c7d3ac0
Robert Jackson <robert.w.jackson@me.com>
2013-06-12 16:18:11 -0400

    Use bundler for tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 ENV['RACK_ENV'] ||= 'test'
 
+require "bundler/setup"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
