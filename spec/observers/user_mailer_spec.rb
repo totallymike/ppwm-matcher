6542d5b39de254bb71a0d71e5303941376e02320
Robert Jackson <robert.w.jackson@me.com>
2013-06-14 12:40:27 -0400

    Properly handle User#name being nil.

--- spec/observers/user_mailer_spec.rb
+++ spec/observers/user_mailer_spec.rb
@@ -18,12 +18,10 @@ module PpwmMatcher
     end
 
     context "when given two users" do
-      let(:users) do
-        [ double("User", name: 'Tim Bower', email: 'tim@example.com', github_login: 'timmy') ,
-          double("User", name: 'Bob Timberland', email: 'bob@example.com', github_login: 'bobby') ]
-      end
-
       it "expects mail to be sent" do
+        users = [ double("User", name: 'Tim Bower', email: 'tim@example.com', github_login: 'timmy') ,
+                  double("User", name: 'Bob Timberland', email: 'bob@example.com', github_login: 'bobby') ]
+
         users.permutation.each do |user, pair|
           mail_client.should_receive(:mail) do |opts|
             opts[:from].should == 'matcher@pairprogramwith.me'
@@ -33,6 +31,20 @@ module PpwmMatcher
             opts[:body].should include("Hi #{user.name},")
             opts[:body].should include("You can email them at #{pair.email}")
             opts[:body].should include("You've been paired up with #{pair.name}")
+          end
+        end
+
+        mailer.users_assigned(users)
+      end
+
+      it "uses github_login if name isn't present" do
+        users = [ double("User", name: nil, email: 'tim@example.com', github_login: 'timmy') ,
+                  double("User", name: nil, email: 'bob@example.com', github_login: 'bobby') ]
+
+        users.permutation.each do |user, pair|
+          mail_client.should_receive(:mail) do |opts|
+            opts[:body].should include("Hi #{user.github_login},")
+            opts[:body].should include("You've been paired up with #{pair.github_login}")
           end
         end
 
